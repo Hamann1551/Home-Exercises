cb9b277545b8996fbdf8f996f467d86e2e065a65
Kirill <kirill.sklyarov.200314@gmail.com>
2024-07-11 11:19:24 +0200

    Homework #10

--- HomeW10/Task 1/src/Main.java
+++ HomeW10/Task 1/src/Main.java
@@ -8,20 +8,26 @@ public class Main {
         while (true) {
             String name = inputBookName();
             int pageCount = inputPageCount();
+            double praceBook = inputPraceBook();
             addBook(name);
             addBook(name, pageCount);
-            //todo использовать новый метод тут
+            addBook(name, pageCount, praceBook);
             printInfo();
         }
     }
 
     public static String inputBookName() {
-        System.out.println("Введите название книги:");
+        System.out.print("Введите название книги:");
         return new Scanner(System.in).nextLine();
     }
 
     public static int inputPageCount() {
-        System.out.println("Введите кол-во страниц:");
+        System.out.print("Введите кол-во страниц:");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static int inputPraceBook() {
+        System.out.print("Введите цену книги в грн: ");
         return new Scanner(System.in).nextInt();
     }
 
@@ -30,11 +36,18 @@ public class Main {
     }
 
     public static void addBook(String bookName, int pageCount) {
-        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
+        //booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
+        addBook(bookName, pageCount, 0);
+    }
+
+    public static void addBook(String bookName, int pageCount, double praceBook){
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр., " + "-> " + "цена: " + (praceBook > 0 ? praceBook : "N/A") + "\n";
     }
 
     public static void printInfo() {
         System.out.println(booksInfo);
     }
+
+
 
 }
